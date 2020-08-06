2fa6ce79ef67cfe9f0ba0c263b12e36f8237d99f
Gregor Riegler <gregor.riegler@ebcont.com>
2020-08-06 20:54:27 +0200

    can restart gnome shell from npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const gulp = require("gulp");
+const exec = require('child_process').exec;
 
 const metadata = JSON.parse(fs.readFileSync("metadata.json"));
 
@@ -11,7 +12,20 @@ var config = {
     installDir: installDir,
 };
 
-gulp.task("install", function () {
-    return gulp.src(["metadata.json", "ding.ogg", "egg.svg", "stylesheet.css", "LICENSE", config.srcDir + "/*.js"])
+function install(cb) {
+    gulp.src(["metadata.json", "ding.ogg", "egg.svg", "stylesheet.css", "LICENSE", config.srcDir + "/*.js"])
         .pipe(gulp.dest(config.installDir));
-});
+    cb();
+}
+
+function restartGnomeShell(cb) {
+    exec("gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval 'Meta.restart(_(\"Restarting…\"))'")
+    cb();
+}
+
+exports.install = install;
+exports.restartGnomeShell = restartGnomeShell;
+exports.start = gulp.series(
+    install,
+    restartGnomeShell
+);
